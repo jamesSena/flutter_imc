a5be5c502e7433a04167568ecf5dc67404e22b2f
Jamesson Sales de Sena <jsdesena@interaxa.com>
2020-01-28 09:10:02 -0300

    Iniciando o desenvolvimento

---
+++ lib/ui/android/pages/home.page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cálculo de IMC"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: "Altura (cm)",
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
